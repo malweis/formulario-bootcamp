6cf9324c4a1f151bd8e4663036669c1cb2bbe877
malweis <58945171+malweis@users.noreply.github.com>
2023-05-16 15:15:36 -0400

    fix: Arregla validaciones
    Validaciones de nro de documento solo tenian que
    ser aplicadas cuando el select elegido era RUC
    la validacion se activa correctamente y se
    desactiva cuando se selecciona otra opcion

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const nroDocInput = document.getElementById('nroDocumento');
-    const selectElements = document.querySelectorAll('select');
-    const form = document.querySelector('form');
-  
-    nroDocInput.addEventListener('input', function () {
-      const inputValue = this.value;
-      const regex = /^\d+-\d$/; // Matches format: X numbers - one number
-  
+  const nroDocInput = document.getElementById('nroDocumento');
+  const selectElements = document.querySelectorAll('select');
+  const tipoDocumentoSelect = document.getElementById('tipoDocumento');
+  const form = document.querySelector('form');
+
+  nroDocInput.addEventListener('input', function () {
+    const inputValue = this.value;
+    const regex = /^\d+-\d$/; // Matches format: X numbers - one number
+
+    if (tipoDocumentoSelect.value === '2') {
       if (!regex.test(inputValue)) {
         this.setCustomValidity('Invalid format. Please use the format X numbers - one number (e.g., 12345-6).');
         this.classList.add('error');
@@ -14,46 +16,54 @@ document.addEventListener('DOMContentLoaded', function () {
         this.setCustomValidity('');
         this.classList.remove('error');
       }
-    });
-  
-    selectElements.forEach(function(select) {
-      // Remove 'error' class when an option is selected
-      select.addEventListener('change', function() {
-        const selectedValue = this.value;
-  
-        if (selectedValue !== '') {
-          this.classList.remove('error');
-          this.setCustomValidity('');
-        }
-      });
-    });
-  
-    form.addEventListener('submit', function (event) {
-      let formIsValid = true;
-  
-      selectElements.forEach(function(select) {
-        const selectedValue = select.value;
-  
-        if (selectedValue === '') {
-          select.setCustomValidity('Por favor seleccione una opcion.');
-          select.classList.add('error');
-          select.reportValidity(); // Trigger the browser's validity message
-          formIsValid = false;
-        } else {
-          select.setCustomValidity('');
-        }
-      });
-  
-      if (!formIsValid) {
-        event.preventDefault(); // Prevent form submission
-      }
-    });
-  
-    // Add event listener to validate form when form inputs are updated
-    form.addEventListener('input', function() {
-      if (form.checkValidity()) {
-        formIsValid = true;
+    } else {
+      this.setCustomValidity('');
+      this.classList.remove('error');
+    }
+  });
+
+  tipoDocumentoSelect.addEventListener('change', function() {
+    if (this.value !== '2') {
+      nroDocInput.setCustomValidity('');
+      nroDocInput.classList.remove('error');
+    }
+  });
+
+  selectElements.forEach(function(select) {
+    select.addEventListener('change', function() {
+      const selectedValue = this.value;
+
+      if (selectedValue !== '') {
+        this.classList.remove('error');
+        this.setCustomValidity('');
       }
     });
   });
-  
+
+  form.addEventListener('submit', function (event) {
+    let formIsValid = true;
+
+    selectElements.forEach(function(select) {
+      const selectedValue = select.value;
+
+      if (selectedValue === '') {
+        select.setCustomValidity('Por favor seleccione una opcion.');
+        select.classList.add('error');
+        select.reportValidity();
+        formIsValid = false;
+      } else {
+        select.setCustomValidity('');
+      }
+    });
+
+    if (!formIsValid) {
+      event.preventDefault();
+    }
+  });
+
+  form.addEventListener('input', function() {
+    if (form.checkValidity()) {
+      formIsValid = true;
+    }
+  });
+});
